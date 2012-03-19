5d66b23ded1f9c7845f3ce9148599a558952c684
Jimmy Hillis <ppjim3@gmail.com>
2012-03-20 00:02:50 +0800

    Setting up the app to be much more contained with no closures on the Comic/Series objects

--- program.js
+++ program.js
@@ -1,77 +1,71 @@
-// COMIC BOOK JAVASCRIPT APPLICATION
-// This is a practice application that allows me to
-// build and work through a basic Comic Book database
-// made up of a number of Objects which are linked 
-// together. The purpose being the ability to propagate
-// and load all related comics.
+/*
 
-(function JSCOMICBOX() {
+JS COMIC BOX
 
-	// Create some basic required prototypes
-	String.prototype.trim = function() {
-		return this.replace(/^s+|\s+$/g, '');
-	}
+This is a practice application that allows me to
+build and work through a basic Comic Book database
+made up of a number of Objects which are linked 
+together. The purpose being the ability to propagate
+and load all related comics. 
 
-	// Simple write function which will allow me to
-	// write directly to my prettified HTML markup.
-	// Hack, but was sick of looking at an ugly page!
-	document.write = function(str) {
-		var content = document.getElementById('content');
+Developer: Jimmy Hillis / jimmy.hillis@me.com
+Repository: https://github.com/jimmyhillis/JSComicBox/
+Version: 0.1
+Last Update: 19/03/2012
+
+*/
+
+// Default settings for loading the JSComicBox app
+var s = { 'output_element':'content' };
+
+(function JsComicBox(settings) {
+
+	var comicbox = {}, 
+		comics = [], // array of comics in your personal
+		comic_series = []; // array of comic series e.g. Amazing Spider-Man (1999)
+
+	comicbox._version = "0.1";
+	comicbox._developer = "jimmy.hillis@me.com";
+	comicbox.output_element = (typeof settings.output_element == String) ?
+		settings.output_element :
+		'content';
+
+	// Function allows me to write simple markup (generally HTML) to the browser
+	// for client + testing purposes
+	output = function(str) {
+		var content = document.getElementById(comicbox.output_element);
 		var element = document.createElement('p');
 		element.innerHTML = str;
 		content.appendChild(element);
 	}
 
-	// Create array of comics and comic series
-	// ... basically the "database"
-	this.comics = [];
-	this.comic_series = [];
+	/* == OBJECT TYPES FOR COMIC BOX == */
 
-	// Object format for a single comic issue
-	this.Comic = function(series, issue, year) {
-		return {
-			'series': series || 'Amazing Spider-Man',
-			'issue': issue || 1,
-			'year': year || 1969,
-			'getTitle': function() {
-				return ((typeof this.series === "object") ? this.series.getTitle() : this.series) + ' #' + this.issue;
-			}
-		}
+	// Comic constructor
+	Comic = function(series, issue, year) {
+		this.series = series || 'Amazing Spider-Man',
+		this.issue = issue || 1,
+		this.year = year || 1969
 	}
-
-	// Object format for a Comic Book Series (eg. The Amazing Spider-Man (1999))
-	this.Series = function(name, year, publisher) {
-		return {
-			'name': name,
-			'year': year,
-			'publisher': publisher,
-			'getTitle': function() {
-				return this.name + ' (' + this.year + ')';
-			}
-		}
-	}
-
-	this.ComicSeries = function() {
-		return {
-			'getTitle': function() {
-				return this.name + ' (' + this.year + ')';
-			}
-		}
+	// Return the title of the comic issue based on SERIES #ISSUE {YEAR}
+	Comic.prototype.getTitle = function() {
+		return ((typeof this.series === "object") ? 
+			this.series.getTitle() : 
+			this.series) + ' #' + this.issue + ' [' + this.year + ']';
 	};
 
-	// Add a series listing to the Application
-	this.list_series = function() {
-		var list = "";
-		list += '<ul>';
-		// Loop through each series and spit out the name
-		for (var i = this.comic_series.length - 1; i >= 0; i--) {
-			list += '<li>' + this.comic_series[i].getTitle() + '</li>';
-		};
-		list += '</ul>';
-		document.write(list);
+	// Comic series constructor {e.g. The Amazing Spider-Man (1999)}
+	Series = function(name, year, publisher) {
+		this.name = name;
+		this.year = year;
+		this.publisher = publisher;
+	};
+	// Add prototypal functions to the Comic Series class
+	Series.prototype.getTitle = function() {
+		return this.name + ' [' + this.year + ']';
 	}
 
-	// UX HANDLERS
+	// !=== UX HANDLERS -- SHOULD NOT BE IN THIS APP? === */
 
 	// Load specific requirements
 	var newComic = document.getElementById('new_comic_record');
@@ -82,19 +76,19 @@
 	var newComicCallback = function() {
 		console.log('New record, let\'s start here!');
 		var existing_series = false;
-		this.comic_series.forEach(function(element,index) { 
+		comic_series.forEach(function(element,index) { 
 			if(element.name === newComicElements.new_series.value) {
 				existing_series = index;
 			}
 		});
 		if(existing_series === false) {
-			existing_series = this.comic_series.push(
-				new this.Series(newComicElements.new_series.value, newComicElements.new_year.value)) - 1;
+			existing_series = comic_series.push(
+				new Series(newComicElements.new_series.value, newComicElements.new_year.value)) - 1;
 		}
 		// Add new comic to the database
-		var new_comic_id = this.comics.push(new this.Comic(this.comic_series[existing_series].name, newComicElements.new_issue.value, newComicElements.new_year.value));
+		var new_comic_id = comics.push(new Comic(comic_series[existing_series].name, newComicElements.new_issue.value, newComicElements.new_year.value));
 		// Display the title to the end of our list
-		document.write(this.comics[new_comic_id-1].getTitle().trim() + '<br />');
+		output(comics[new_comic_id-1].getTitle().trim() + '<br />');
 		// Stop browser acting on submit with page refresh
 		return false;
 	}
@@ -107,30 +101,45 @@
 		return false;
 	}
 
-	// MAIN CODE AND TESTS FOR THE ABOVE CODE
+	/* !=== END BAD CODE HERE ===1 */
 
-	this.main = function() {
+	/* == COMIC BOX PUBLIC METHODS == */
 
+	// Add a series listing to the Application
+	comicbox.list_series = function() {
+		var list = "";
+		list += '<ul>';
+		// Loop through each series and spit out the name
+		for (var i = comic_series.length - 1; i >= 0; i--) {
+			list += '<li>' + comic_series[i].getTitle() + '</li>';
+		};
+		list += '</ul>';
+		output(list);
+	}
+
+	// Launch initial comic box functionality and list
+	comicbox.main = function() {
+
+		// Search for existing database within localStorage
 		if(localStorage.getItem('comic_series')) {
-			document.write('Loading existing database...');
+			output('Loading existing database...');
 			JSON.parse(localStorage.getItem('comic_series')).forEach(function(element, index) {
-				this.comic_series.push(new Series(element.name, element.year, element.publisher));
+				comic_series.push(new Series(element.name, element.year, element.publisher));
 			});
-
-		} else {
+		} 
+		// Nothing found, so create a basic example database
+		else {
 			alert('No DB exists, so I\'ll create a demo for you');
-			this.comic_series.push(new this.Series('Uncanny X-Men', 1969, 'Marvel'));
-			this.comic_series.push(new this.Series('Fantastic Four', 1962, 'Marvel'));
+			comic_series.push(new Series('Uncanny X-Men', 1969, 'Marvel'));
+			comic_series.push(new Series('Fantastic Four', 1962, 'Marvel'));
 			// Store the current Comic Series database into LocalStorage for persistance
-			localStorage.setItem('comic_series', JSON.stringify(this.comic_series));
+			localStorage.setItem('comic_series', JSON.stringify(comic_series));
 		}
 		
-		this.list_series();
-		
-		//for (var i = this.comics.length - 1; i >= 0; i--) {
-		//	document.write(this.comics[i].getTitle().trim() + '<br />');
-		//};
-		
-	}();
+		comicbox.list_series();
+				
+	};
 
-}());
+	return comicbox;
+
+}(s)).main();
