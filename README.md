862aa7cb3afa411993500ac3938ed2b270b59397
Jimmy Hillis <ppjim3@gmail.com>
2012-03-22 23:57:48 +0800

    Working code to add Series and Comics. Better code speration and style

---
+++ README.md
@@ -0,0 +1,6 @@
+COMIC BOX
+---------
+
+==PURPOSE
+
+This vanilla JavaScript app is a testing bed for me to practice my JavaScript skills while studying and learning about the language and the past practices within that. It's applicative purpose is to be a working and persistant Comic Book tracking system that maps series, issues, etc. using pure JavaScript, JSON storage and localStorage.
--- program.js
+++ program.js
@@ -14,6 +14,7 @@ Version: 0.1
 Last Update: 19/03/2012
 
 */
+"use_strict"
 
 // Default settings for loading the JSComicBox app
 var s = { 'output_element':'content' };
@@ -32,7 +33,7 @@ var app = (function JsComicBox(settings) {
 
 	// Function allows me to write simple markup (generally HTML) to the browser
 	// for client + testing purposes
-	output = function(str) {
+	var output = function(str) {
 		var content = document.getElementById(comicbox.output_element);
 		var element = document.createElement('p');
 		element.innerHTML = str;
@@ -42,35 +43,36 @@ var app = (function JsComicBox(settings) {
 	/* == OBJECT TYPES FOR COMIC BOX == */
 
 	// Comic constructor
-	Comic = function(series, issue, year) {
+	var Comic = function(series, issue, year) {
 		this.series = series || 'Amazing Spider-Man',
 		this.issue = issue || 1,
 		this.year = year || 1969
 	}
 	// Return the title of the comic issue based on SERIES #ISSUE {YEAR}
-	Comic.prototype.getTitle = function() {
+	Comic.prototype.get_title = function() {
 		return ((typeof this.series === "object") ? 
-			this.series.getTitle() : 
+			this.series.get_title() : 
 			this.series) + ' #' + this.issue + ' [' + this.year + ']';
 	};
 
 	// Comic series constructor {e.g. The Amazing Spider-Man (1999)}
-	Series = function(name, year, publisher) {
+	var Series = function(name, year, publisher) {
 		this.name = name;
 		this.year = year;
 		this.publisher = publisher;
 	};
 	// Add prototypal functions to the Comic Series class
-	Series.prototype.getTitle = function() {
+	Series.prototype.get_title = function() {
 		return this.name + ' [' + this.year + ']';
 	}
 
 	/* == COMIC BOX PUBLIC METHODS == */
 
+	// Add a series listing to the comic box
 	comicbox.add_series = function(name, year, publisher) {
 		// Check if series already exists, and return that if so
 		comic_series.forEach(function(element,index) { 
-			if(element.name === newComicElements.new_series.value) {
+			if(element.name === name) {
 				return element;
 			}
 		});
@@ -79,21 +81,53 @@ var app = (function JsComicBox(settings) {
 		return comic_series[index-1];
 	}
 
-	// Add a series listing to the Application
+	// Output a list of all the current series
 	comicbox.list_series = function() {
 		var list = "";
 		list += '<ul>';
 		// Loop through each series and spit out the name
 		for (var i = comic_series.length - 1; i >= 0; i--) {
-			list += '<li>' + comic_series[i].getTitle() + '</li>';
+			list += '<li>' + comic_series[i].get_title() + '</li>';
 		};
 		list += '</ul>';
 		output(list);
+		return this;
+	}
+
+	// Add a comic to the comic box
+	comicbox.add_comic = function(series, issue) {
+		// Validate input
+		if (!series || !issue) {
+			return false;
+		}
+		// Look for an index and load the actual object
+		if (typeof series === Number) {
+			series = comic_series[series];
+		}
+		index = comics.push(new Comic(series, issue));
+		return comics[index-1];
+	}
+
+	// Output a list of all the current series
+	comicbox.list_comics = function() {
+		// @todo priority this listing function needs to be written correctly
+		console.log(comics);
+		return this;
+	}
+
+	// Save the current working "database" for persistance (localStorage)
+	comicbox.commit = function() {
+		if (comic_series.length > 0) {
+			localStorage.setItem('comic_series', JSON.stringify(comic_series));
+		}
+		if (comics.length > 0) {
+			localStorage.setItem('comic', JSON.stringify(comics));
+		}
+		return this;
 	}
 
 	// Launch initial comic box functionality and list
 	comicbox.main = function() {
-
 		// Search for existing database within localStorage
 		if(localStorage.getItem('comic_series')) {
 			output('Loading existing database...');
@@ -109,9 +143,7 @@ var app = (function JsComicBox(settings) {
 			// Store the current Comic Series database into LocalStorage for persistance
 			localStorage.setItem('comic_series', JSON.stringify(comic_series));
 		}
-		
 		comicbox.list_series();
-				
 	};
 
 	return comicbox;
@@ -126,32 +158,62 @@ app.main();
 // application, without having loosley related code inside there.
 // This will split the presentation + the functionality in a more
 // MVC manner (at least it's a start!)
+(function() {
+	// Load specific requirements
+	var newRecordsForm = document.getElementById('new_records');
 
-// Load specific requirements
-var newComic = document.getElementById('new_comic_record');
-var newComicElements = newComic.elements;
+	// This event listener adds a new SERIES to the database
+	// from the user input, once validataed
+	var newSeriesCallback = function() {
+		// Function variables for the new series + user input
+		var new_series,
+			title = newRecordsForm.new_series_name.value || false,
+			year = newRecordsForm.new_series_year.value || false;
+		// Very basic form validation to confirm they are entered
+		if(!title || !year) {
+			alert('You must enter in all fields before adding a new Series');
+			return false;
+		}
+		var new_series = app.add_series(title, year);
+		console.log('Added a new Series: ' + new_series.get_title());
+		return false;
+	};
 
-// Add listener for a submit, and make sure no page reload happens
-var newComicCallback = function() {
-	var title = newComicElements.new_series.value || false,
-		year = newComicElements.new_year.value || false;
-	// Very basic form validation to confirm they are entered
-	if(!title || !year) {
-		alert('You must enter in all fields before adding a new Series');
+	// This event listener adds a new COMIC to the database
+	// from the user input, once validataed
+	// @purpose
+	// @return 
+	var newComicCallback = function() {
+		// Function variables for the new series + user input
+		var new_series,
+			series = newRecordsForm.new_comic_series.value || false,
+			issue = newRecordsForm.new_comic_issue.value || false;
+		// Very basic form validation to confirm they are entered
+		if(!series || !issue) {
+			alert('You must enter in all fields before adding a new Comic');
+			return false;
+		}
+		var new_comic = app.add_comic(series, issue);
+		console.log('Added a new Comic: ' + new_comic.get_title());
+		return true;
+	};
+
+	// Commit callback
+	// @purpose This function will commit the current status of the database
+	// @return APP for chaining
+	var commitCallback = function() { 
+		app.commit();
+		return app;
+	};
+	// Attach events to form listeners to the submit button
+	// and cancel out the form from refreshing, if there is a
+	// JS error
+	newRecordsForm.new_series_submit.onclick = newSeriesCallback;
+	newRecordsForm.new_comic_submit.onclick = newComicCallback;
+	newRecordsForm.commit.onclick = commitCallback;
+	// Stop the form from submitting, in the event of a crash
+	// on the ADD NEW code, should use TRY/CATCH
+	newRecordsForm.onsubmit = function() {
 		return false;
 	}
-	var newseries = app.add_series(newComicElements.new_series.value, newComicElements.new_year.value);
-	console.log('Added a new Series: ' + newseries.getTitle());
-	return false;
-}
-
-// Attach events to form listeners to the submit button
-// and cancel out the form from refreshing, if there is a
-// JS error
-newComic.new_comic.onclick = newComicCallback;
-// Stop the form from submitting, in the event of a crash
-// on the ADD NEW code, should use TRY/CATCH
-document.getElementById('new_comic').onsubmit = function() {
-	return false;
-}
-
+}());
